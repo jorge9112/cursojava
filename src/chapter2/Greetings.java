98974cedacd57158a06d87203a76a3155c964e9d
Prueba <a_herrerajol@globalhitss.com>
2020-12-11 14:23:55 -0600

    metodosespañol

--- src/chapter2/Greetings.java
+++ src/chapter2/Greetings.java
@@ -6,13 +6,13 @@ public class Greetings {
 
     public static void main(String args[]){
 
-        System.out.println("Enter your name:");
+        System.out.println("Ingresa tu nombre:");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         greetUser(name);
 
     }
     public static void greetUser(String name){
-        System.out.println("Hi there, " + name);
+        System.out.println("Hola, " + name);
     }
 }

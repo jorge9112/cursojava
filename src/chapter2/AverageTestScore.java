617702345a400f5ba7e7839a28057044ed72f7d3
Prueba <a_herrerajol@globalhitss.com>
2020-12-11 13:22:52 -0600

    bucle anidados

---
+++ src/chapter2/AverageTestScore.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class AverageTestScore {
+    public static void main(String[]){
+
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i=0; i< numberOfStudents; i++){
+
+            double total = 0;
+            for(int j=0; j<numberOfTests; j++){
+                System.out.println("Enter the score for Test #" + (j+1));
+                double score = scanner.nextDouble();
+                total = total + score;
+            }
+
+           double average = total/numberOfTests;
+            System.out.println("The test average for student #" + (i+1) + " is " + average);
+
+        }
+
+        scanner.close();
+    }
+}

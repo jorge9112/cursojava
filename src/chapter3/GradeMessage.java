03b6f79e2c214daa61d55c26d6a96ca96f945c2e
Prueba <a_herrerajol@globalhitss.com>
2020-12-11 09:47:53 -0600

    declaraciones de cambio

---
+++ src/chapter3/GradeMessage.java
@@ -0,0 +1,36 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+    public static void main(String args[]){
+
+        System.out.println("Enter yor later grade");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+
+        String message;
+
+        switch(grade){
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+            case "D":
+                message = "Your need to work a bit harder";
+                break;
+            case "F":
+                message = "Uh oh";
+                break;
+            default:
+                message = "Error. Invalid grade";
+                break;
+        }
+        System.out.println(message);
+    }
+}
